cc632d392cf44db9803d019e60b3042d6ab3df5e
downvoid <161539698+downvoid@users.noreply.github.com>
2025-03-01 15:42:24 -0500

    fix bad tests

--- test/h-group/variants/muddy-convs.js
+++ test/h-group/variants/muddy-convs.js
@@ -21,6 +21,21 @@ describe('save clue interpretation', () => {
 
 		takeTurn(game, 'Bob clues red to Alice (slot 5)');
 
+		assert.ok(['m2', 'm5'].every(id =>
+			game.common.thoughts[game.state.hands[PLAYER.ALICE][4]].inferred.has(expandShortCard(id))));
+	});
+	it('understands red saves in cocoa rainbow', () => {
+		const game = setup(HGroup, [
+			['xx', 'xx', 'xx', 'xx', 'xx'],
+			['g2', 'b1', 'r2', 'r3', 'g5'],
+		], {
+			level: { min: 1 },
+			starting: PLAYER.BOB,
+			variant: VARIANTS.COCOA_RAINBOW
+		});
+
+		takeTurn(game, 'Bob clues red to Alice (slot 5)');
+
 		assert.ok(['m2', 'm3', 'm4', 'm5'].every(id =>
 			game.common.thoughts[game.state.hands[PLAYER.ALICE][4]].inferred.has(expandShortCard(id))));
 	});
