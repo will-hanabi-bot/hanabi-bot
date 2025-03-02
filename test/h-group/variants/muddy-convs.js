e1ef1fe9906e75a969cb89e440aa0b2b2c508db4
downvoid <161539698+downvoid@users.noreply.github.com>
2025-03-02 00:07:23 -0500

    import the ExAsserts

--- test/h-group/variants/muddy-convs.js
+++ test/h-group/variants/muddy-convs.js
@@ -1,6 +1,6 @@
 import { strict as assert } from 'node:assert';
 import { describe, it } from 'node:test';
-
+import * as ExAsserts from '../../extra-asserts.js';
 import { PLAYER, VARIANTS, expandShortCard, setup, takeTurn } from '../../test-utils.js';
 import HGroup from '../../../src/conventions/h-group.js';
 
