570df06e405d623b4aa6796910780e620920c4d6
downvoid <161539698+downvoid@users.noreply.github.com>
2025-03-01 22:25:46 -0500

    Add test for skipping over non-muddy cards

--- test/h-group/variants/muddy-convs.js
+++ test/h-group/variants/muddy-convs.js
@@ -75,4 +75,43 @@ describe('muddy tempo clues', () => {
 		assert.ok(['m2', 'm5'].every(id =>
 			!game.common.thoughts[game.state.hands[PLAYER.ALICE][3]].inferred.has(expandShortCard(id))));
 	});
+	it('wraps around', () => {
+		const game = setup(HGroup, [
+			['g1', 'g1', 'g1', 'm1', 'm5'],
+			['g2', 'b1', 'r2', 'r3', 'g5'],
+			['g2', 'b1', 'r2', 'r3', 'y5'],
+		], {
+			level: { min: 1 },
+			starting: PLAYER.BOB,
+			variant: VARIANTS.MUDDY_RAINBOW
+		});
+
+		takeTurn(game, 'Bob clues red to Alice (slots 4,5)');
+		takeTurn(game, 'Cathy clues yellow to Alice (slots 4,5)');
+
+		assert.ok(['m1'].every(id =>
+			game.common.thoughts[game.state.hands[PLAYER.ALICE][3]].inferred.has(expandShortCard(id))));
+		assert.ok(['m2', 'm3', 'm4', 'm5'].every(id =>
+			!game.common.thoughts[game.state.hands[PLAYER.ALICE][3]].inferred.has(expandShortCard(id))));
+	});
+	it('skips over known non-muddy cards', () => {
+		const game = setup(HGroup, [
+			['m3', 'm1', 'b5', 'g1'],
+			['b1', 'r2', 'r3', 'g5'],
+			['b1', 'r2', 'r3', 'y5'],
+			['b1', 'r2', 'r3', 'y5'],
+		], {
+			level: { min: 1 },
+			starting: PLAYER.BOB,
+			variant: VARIANTS.MUDDY_RAINBOW
+		});
+
+		takeTurn(game, 'Bob clues 5 to Alice (slot 3)');
+		takeTurn(game, 'Cathy clues green to Alice (slots 1,2,4)');
+		takeTurn(game, 'Donald clues blue to Alice (slots 1,2,3)');
+		
+
+		assert.ok(['m1'].every(id =>
+			game.common.thoughts[game.state.hands[PLAYER.ALICE][1]].inferred.has(expandShortCard(id))));
+	});
 });
