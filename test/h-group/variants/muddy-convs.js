ed2b856a29056269b5e756f879550c5de29df0b9
downvoid <161539698+downvoid@users.noreply.github.com>
2025-03-02 01:18:07 -0500

    Update muddy-convs.js

--- test/h-group/variants/muddy-convs.js
+++ test/h-group/variants/muddy-convs.js
@@ -35,8 +35,7 @@ describe('save clue interpretation', () => {
 
 		takeTurn(game, 'Bob clues red to Alice (slot 5)');
 
-		assert.ok(['m2', 'm3', 'm4', 'm5'].every(id =>
-			game.common.thoughts[game.state.hands[PLAYER.ALICE][4]].inferred.has(expandShortCard(id))));
+		ExAsserts.cardHasInferences(game.common.thoughts[game.state.hands[PLAYER.ALICE][4]], ['r1', 'm1', 'm2', 'm3', 'm4', 'm5']);
 	});
 	it('does not interpret other colors as saves', () => {
 		const game = setup(HGroup, [
@@ -69,10 +68,7 @@ describe('muddy tempo clues', () => {
 		takeTurn(game, 'Bob clues red to Alice (slots 2,3,4,5)');
 		takeTurn(game, 'Cathy clues green to Alice (slots 3,4,5)');
 
-		assert.ok(['m1'].every(id =>
-			game.common.thoughts[game.state.hands[PLAYER.ALICE][3]].inferred.has(expandShortCard(id))));
-		assert.ok(['m2', 'm5'].every(id =>
-			!game.common.thoughts[game.state.hands[PLAYER.ALICE][3]].inferred.has(expandShortCard(id))));
+		ExAsserts.cardHasInferences(game.common.thoughts[game.state.hands[PLAYER.ALICE][4]], ['m1']);
 	});
 	it('wraps around', () => {
 		const game = setup(HGroup, [
@@ -88,10 +84,7 @@ describe('muddy tempo clues', () => {
 		takeTurn(game, 'Bob clues yellow to Alice (slots 3,4,5)');
 		takeTurn(game, 'Cathy clues red to Alice (slots 3,4)');
 
-		assert.ok(['m1'].every(id =>
-			game.common.thoughts[game.state.hands[PLAYER.ALICE][2]].inferred.has(expandShortCard(id))));
-		assert.ok(['m2', 'm3', 'm4', 'm5'].every(id =>
-			!game.common.thoughts[game.state.hands[PLAYER.ALICE][2]].inferred.has(expandShortCard(id))));
+		ExAsserts.cardHasInferences(game.common.thoughts[game.state.hands[PLAYER.ALICE][2]], ['m1']);
 	});
 	it('skips over known non-muddy cards', () => {
 		const game = setup(HGroup, [
@@ -110,7 +103,6 @@ describe('muddy tempo clues', () => {
 		takeTurn(game, 'Donald clues blue to Alice (slots 1,2,3)');
 		
 
-		assert.ok(['m1'].every(id =>
-			game.common.thoughts[game.state.hands[PLAYER.ALICE][1]].inferred.has(expandShortCard(id))));
+		ExAsserts.cardHasInferences(game.common.thoughts[game.state.hands[PLAYER.ALICE][1]], ['m1']);
 	});
 });
