6b9b92f46addc51ebd79b482f5ccd6bc38481f18
d20cay <github.com.imaginary068@passmail.net>
2025-09-15 01:00:09 +0200

    exports const.

--- src/constants.js
+++ src/constants.js
@@ -42,6 +42,6 @@ export const ENDGAME_SOLVING_FUNCS = {
 };
 
 export const HANABI_HOSTNAME = process.env['HANABI_HOSTNAME'] || 'hanab.live';
-SSL_ENABLED = undefined === process.env['SSL_ENABLED'] || 'true' === process.env['SSL_ENABLED'];
+export const SSL_ENABLED = undefined === process.env['SSL_ENABLED'] || 'true' === process.env['SSL_ENABLED'];
 export const HANABI_PORT = parseInt(process.env['HANABI_PORT']) || (SSL_ENABLED ? 443 : 80);
 export const CUSTOM_PORT = 443 !== HANABI_PORT && 80 !== HANABI_PORT;
